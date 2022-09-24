fee5cdd584f115789e21b0f094a23b8f6c06c31e
liliankim <yejean831@gmail.com>
2022-09-23 21:59:43 -0700

    completed reactions.js and thought.js

--- Develop/models/thought.js
+++ Develop/models/thought.js
@@ -1,14 +1,35 @@
 const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./reaction');
 
-const thoughtSchema = new Schema (
+const thoughtSchema = new Schema(
     {
-    thoughtText:{
-        type: String,
-        required: true,
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+
+    {
+        toJSON: {
+            getters: true,
+        },
+
     }
+);
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length
+})
+const Thought = model('thought',thoughtSchema);
 
-
-
-
-    }
-)
+moduel.exports = Thought;
